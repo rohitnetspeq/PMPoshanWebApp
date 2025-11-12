4474065e105a48897fb51d2229085504849dd4f2
rohitnetspeq <rohit.bhagat@netspeq.com>
2025-11-12 17:46:47 +0530

    font accessiblity implemented

---
+++ PMPoshanWebApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMPoshanWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
